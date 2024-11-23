850ed351c02a0720724cf54db57404a0d1a56cbb
Cecil Thomas <11084542+kitsune21@users.noreply.github.com>
2024-11-23 11:23:37 -0500

    Revert "changed to say levels"
    This reverts commit f9d6626e188792530c622fc4adb4de51e75e3c50.

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import PracticeButton from "./comps/practiceButton";
 import Game from "./game";
 
 function App() {
-  const easy = "easy";
-  const med = "med";
-  const hard = "hard";
+  const easy = 'easy';
+  const med = 'med';
+  const hard = 'hard';
   const [selectedGame, setSelectedGame] = useState(0);
-  const [difficulty, setDifficulty] = useState<"easy" | "med" | "hard">(med);
+  const [difficulty, setDifficulty] = useState<'easy' | 'med' | 'hard'>(med);
 
   function reset() {
     setSelectedGame(0);
   }
 
-  function handleDifficulty(level: "easy" | "med" | "hard") {
+  function handleDifficulty(level: 'easy' | 'med' | 'hard') {
     setDifficulty(level);
   }
 
@@ -21,34 +21,14 @@ function App() {
     <section className="flex items-center py-10 flex-col">
       {selectedGame === 0 ? (
         <div className="flex p-4 flex-col">
-          <h1 className="text-2xl underline text-center">
-            Welcome to Em Math!
-          </h1>
+          <h1 className="text-2xl underline text-center">Welcome to Em Math!</h1>
           <h2 className="text-xl">
             Pick which times tables you would like to practice:
           </h2>
           <div className="flex justify-center py-4">
-            <button
-              data-difficulty={difficulty === easy}
-              className="border-2 border-r-0 border-black rounded-l-xl bg-gray-200 hover:bg-gray-300 hover:underline p-2 transition-all duration-200 data-[difficulty=true]:bg-sky-400"
-              onClick={() => handleDifficulty(easy)}
-            >
-              Level 1
-            </button>
-            <button
-              data-difficulty={difficulty === med}
-              className="border-y-2 border-black bg-gray-200 hover:bg-gray-300 hover:underline p-2 transition-all duration-200 data-[difficulty=true]:bg-sky-400"
-              onClick={() => handleDifficulty(med)}
-            >
-              Level 2
-            </button>
-            <button
-              data-difficulty={difficulty === hard}
-              className="border-2 border-l-0 border-black rounded-r-xl bg-gray-200 hover:bg-gray-300 hover:underline p-2 transition-all duration-200 data-[difficulty=true]:bg-sky-400"
-              onClick={() => handleDifficulty(hard)}
-            >
-              Level 3
-            </button>
+            <button data-difficulty={difficulty === easy} className="border-2 border-r-0 border-black rounded-l-xl bg-gray-200 hover:bg-gray-300 hover:underline p-2 transition-all duration-200 data-[difficulty=true]:bg-sky-400" onClick={() => handleDifficulty(easy)}>Easy</button>
+            <button data-difficulty={difficulty === med} className="border-y-2 border-black bg-gray-200 hover:bg-gray-300 hover:underline p-2 transition-all duration-200 data-[difficulty=true]:bg-sky-400" onClick={() => handleDifficulty(med)}>Medium</button>
+            <button data-difficulty={difficulty === hard} className="border-2 border-l-0 border-black rounded-r-xl bg-gray-200 hover:bg-gray-300 hover:underline p-2 transition-all duration-200 data-[difficulty=true]:bg-sky-400" onClick={() => handleDifficulty(hard)}>Hard</button>
           </div>
           <div className="flex flex-col gap-2 py-4">
             <div className="flex gap-2 justify-evenly">
