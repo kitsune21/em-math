e8f4b446165ee0e5b9529f1f813b72f2688a6c5a
Cecil Thomas <11084542+kitsune21@users.noreply.github.com>
2024-11-23 10:51:14 -0500

    updated some more game logic stuff

--- src/comps/practiceButton.tsx
+++ src/comps/practiceButton.tsx
@@ -11,7 +11,7 @@ export default function PracticeButton({
 }: PracticeButtonProps) {
   return (
     <button
-      className="border-2 border-blue-500 rounded-2xl p-4 bg-blue-200 w-24 hover:bg-blue-300 transition-all duration-300 hover:underline"
+      className="border-2 border-cyan-500 rounded-2xl p-4 bg-cyan-200 w-24 hover:bg-cyan-300 transition-all duration-300 hover:underline"
       onClick={() => selectGame(myNum)}
     >
       {myNum}
--- src/game.tsx
+++ src/game.tsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 type GameProps = {
   myNum: number;
   reset: () => void;
+  difficulty: 'easy' | 'med' | 'hard';
 };
 
-export default function Game({ myNum, reset }: GameProps) {
+export default function Game({ myNum, reset, difficulty }: GameProps) {
   const gameCountDown = 5;
+  const [gameTimer, setGameTimer] = useState(0);
   const [startGame, setStartGame] = useState(false);
   const [seconds, setSeconds] = useState(gameCountDown);
   const [score, setScore] = useState(0);
@@ -14,20 +16,33 @@ export default function Game({ myNum, reset }: GameProps) {
   const [gameFinished, setGameFinished] = useState(false);
   const [rightWrongList, setRightWrongList] = useState<boolean[]>([]);
   const [shuffledMathList, setShuffledMathList] = useState<number[]>([]);
+  const formRef = useRef(null);
 
   useEffect(() => {
-    if (startGame) return;
+    if(gameFinished) return;
     const countdownInterval = setInterval(() => {
+      if (startGame) {
+        if(gameTimer > 0 && !gameFinished) {
+          setGameTimer(gameTimer - 1);
+          return;
+        } else {
+          correctProblems(null);
+          return;
+        }
+      }
       if (seconds > 0) {
         setSeconds(seconds - 1);
       } else {
         setStartGame(true);
-        const mathList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        setGameTimer(difficulty === 'easy' ? 60 : difficulty === 'med' ? 45 : 30);
+        const mathList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+        const splicePoint = difficulty === 'easy' ? 11 : difficulty === 'med' ? 13 : mathList.length + 1;
+        const mathListActual = mathList.slice(0, splicePoint)
         const tempShuffledMathList: number[] = [];
-        const mathListLength = mathList.length;
+        const mathListLength = mathListActual.length;
         for (let i = 0; i < mathListLength; i++) {
-          const arrayPos = Math.floor(Math.random() * mathList.length);
-          const randomNumber = mathList.splice(arrayPos, 1)[0];
+          const arrayPos = Math.floor(Math.random() * mathListActual.length);
+          const randomNumber = mathListActual.splice(arrayPos, 1)[0];
           tempShuffledMathList.push(randomNumber);
         }
         setShuffledMathList(tempShuffledMathList);
@@ -35,15 +50,16 @@ export default function Game({ myNum, reset }: GameProps) {
     }, 1000);
 
     return () => clearInterval(countdownInterval);
-  }, [seconds, startGame]);
+  }, [seconds, startGame, difficulty, gameTimer, gameFinished]);
 
-  function correctProblems(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+  function correctProblems(e: React.FormEvent<HTMLFormElement> | null) {
+    if (e) e.preventDefault();
+    if (formRef.current === null) return;
     let tempScore = 0;
     const tempRightWrongList = [];
     for (let i = 0; i < shuffledMathList.length; i++) {
       // @ts-expect-error need to fix types here
-      if (parseInt(e.target[i].value) === shuffledMathList[i] * myNum) {
+      if (parseInt(formRef.current[i].value) === shuffledMathList[i] * myNum) {
         tempScore += 1;
         tempRightWrongList.push(true);
       } else {
@@ -74,40 +90,45 @@ export default function Game({ myNum, reset }: GameProps) {
       ) : (
         <div>
           {displayScore ? (
-            <div>
-              <p>
-                Your Score: {score}/{shuffledMathList.length}{" "}
-                {((score / shuffledMathList.length) * 100).toFixed(2)}%
-              </p>
-              <div className="flex gap-2">
+            <div className="flex flex-col gap-2">
+              <div className="flex justify-evenly">
+                <span className="border-2 border-green-500 rounded-2xl p-2 bg-green-200">Your Score: {score}/{shuffledMathList.length}{" "}</span>
+                <span className="border-2 border-green-500 rounded-2xl p-2 bg-green-200">{((score / shuffledMathList.length) * 100).toFixed(2)}%</span>
+                <span className="border-2 border-green-500 rounded-2xl p-2 bg-green-200">Time Left: {gameTimer}</span>
+              </div>
+              <div className="flex justify-around">
                 <button
                   onClick={resetCurrentGame}
-                  className="border-gray-500 border-2 bg-gray-100 p-2 rounded-lg"
+                  className="border-purple-500 border-2 bg-purple-100 p-2 rounded-lg hover:bg-purple-300 transition-all duration-300"
                 >
                   Go Again?
                 </button>
                 <button
                   onClick={reset}
-                  className="border-gray-500 border-2 bg-gray-100 p-2 rounded-lg"
+                  className="border-purple-500 border-2 bg-purple-100 p-2 rounded-lg hover:bg-purple-300 transition-all duration-300"
                 >
                   Go Back
                 </button>
               </div>
             </div>
           ) : null}
-          <form onSubmit={correctProblems}>
+          <form onSubmit={correctProblems} ref={formRef}>
+            {
+              startGame && !gameFinished && <p className="text-center text-2xl">Time: {gameTimer}</p>
+            }
             <div className="grid grid-cols-3 gap-4 items-center py-10">
               {shuffledMathList.map((number, index) => (
                 <div
                   key={number}
                   className="flex justify-evenly items-center gap-1"
                 >
-                  <label className="w-18 text-lg">
+                  <label className="w-18 text-lg" htmlFor={`number-${number}`}>
                     {number} * {myNum} =
                   </label>
                   <input
+                    id={`number-${number}`}
                     type="number"
-                    className="border border-black w-12 rounded-lg py-2 text-center data-[right=true]:border-green-500 data-[right=false]:border-red-500 data-[right=true]:bg-green-300 data-[right=false]:bg-red-300"
+                    className="border border-black w-12 rounded-lg py-2 text-center data-[right=true]:border-green-500 data-[right=false]:border-rose-500 data-[right=true]:bg-green-300 data-[right=false]:bg-rose-300"
                     autoFocus={!index}
                     disabled={gameFinished}
                     data-right={rightWrongList[index]}
@@ -115,21 +136,24 @@ export default function Game({ myNum, reset }: GameProps) {
                 </div>
               ))}
             </div>
-            <div className="flex justify-around">
+            {
+              !gameFinished && startGame &&
+              <div className="flex justify-around">
               <button
                 type="submit"
-                className="text-2xl text-green-500 border border-green-900 bg-green-100 rounded-3xl p-8 hover:bg-green-300 transition-all duration-300"
+                className="text-2xl border border-green-500 bg-green-100 rounded-3xl p-8 hover:bg-green-300 transition-all duration-300 hover:underline"
               >
                 Submit
               </button>
               <button
                 type="button"
-                className="text-2xl text-red-500 border border-red-900 bg-red-100 rounded-3xl p-8 hover:bg-red-300 transition-all duration-300"
+                className="text-2xl border border-red-900 bg-red-100 rounded-3xl p-8 hover:bg-red-300 transition-all duration-300 hover:underline"
                 onClick={reset}
               >
                 Reset
               </button>
             </div>
+            }
           </form>
         </div>
       )}
