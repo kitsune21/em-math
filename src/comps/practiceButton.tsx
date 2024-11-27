e19503fda248f7f8b0ee72ac5f1ba86089d178df
Cecil Thomas <11084542+kitsune21@users.noreply.github.com>
2024-11-26 17:48:04 -0700

    added navigation and routing

--- src/comps/practiceButton.tsx
+++ src/comps/practiceButton.tsx
@@ -3,11 +3,13 @@ import { Dispatch, SetStateAction } from "react";
 type PracticeButtonProps = {
   myNum: number;
   selectGame: Dispatch<SetStateAction<number>>;
+  progress: boolean[];
 };
 
 export default function PracticeButton({
   myNum,
   selectGame,
+  progress,
 }: PracticeButtonProps) {
   return (
     <button
@@ -15,6 +17,24 @@ export default function PracticeButton({
       onClick={() => selectGame(myNum)}
     >
       {myNum}
+      <div className="flex justify-evenly">
+        <div
+          className="size-2 bg-white data-[progress=true]:bg-emerald-500"
+          data-progress={progress[0]}
+        ></div>
+        <div
+          className="size-2 bg-white data-[progress=true]:bg-emerald-500"
+          data-progress={progress[1]}
+        ></div>
+        <div
+          className="size-2 bg-white data-[progress=true]:bg-emerald-500"
+          data-progress={progress[2]}
+        ></div>
+        <div
+          className="size-2 bg-white data-[progress=true]:bg-emerald-500"
+          data-progress={progress[3]}
+        ></div>
+      </div>
     </button>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Game from "./game.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/game/:number/:difficulty/:type" element={<Game />} />
+    </Routes>
+  </BrowserRouter>
+);
